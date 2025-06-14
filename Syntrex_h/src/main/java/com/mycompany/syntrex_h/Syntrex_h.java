69863c3b517d860213e143a61795f897c0cfcb3c
Herberte_Hupp <herbertehupp33@gmail.com>
2025-06-13 21:37:40 -0300

    Add files via upload

---
+++ Syntrex_h/src/main/java/com/mycompany/syntrex_h/Syntrex_h.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.syntrex_h;
+
+/**
+ *
+ * @author h.hupp
+ */
+public class Syntrex_h {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
